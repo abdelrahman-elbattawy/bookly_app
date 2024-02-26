73bca62e3f792a998cbcf4d135d6b64e38be68b5
Abdelrhman Elbattawy <abdo.elbattawy1st@gmail.com>
2024-02-26 04:35:05 +0200

    create styles file & add title to home view

--- lib/features/home/presentation/views/widgets/featured_list_view.dart
+++ lib/features/home/presentation/views/widgets/featured_list_view.dart
@@ -11,7 +11,7 @@ class FeaturedListView extends StatelessWidget {
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
         itemBuilder: (context, index) => const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8),
+          padding: EdgeInsets.only(right: 8),
           child: FeaturedListViewItem(),
         ),
       ),
