f486669d8b58e5220f981a3e0257fb4bd3967511
Abdelrhman Elbattawy <abdo.elbattawy1st@gmail.com>
2024-02-26 04:09:51 +0200

    added featured list view to home view

---
+++ lib/features/home/presentation/views/widgets/featured_list_view.dart
@@ -0,0 +1,20 @@
+import 'package:bookly_app/features/home/presentation/views/widgets/featured_list_view_item.dart';
+import 'package:flutter/material.dart';
+
+class FeaturedListView extends StatelessWidget {
+  const FeaturedListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .25,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) => const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8),
+          child: FeaturedListViewItem(),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/features/home/presentation/views/widgets/featured_list_view_item.dart
@@ -0,0 +1,24 @@
+import 'package:bookly_app/core/utils/assets_data.dart';
+import 'package:flutter/material.dart';
+
+class FeaturedListViewItem extends StatelessWidget {
+  const FeaturedListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: .66,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          image: const DecorationImage(
+            fit: BoxFit.fill,
+            image: AssetImage(
+              AssetsData.testImage1,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
