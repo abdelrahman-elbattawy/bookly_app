6f1ce82874d95b2bc1c8442ce2d8945f706ec5cc
Abdelrhman Elbattawy <abdo.elbattawy1st@gmail.com>
2024-02-27 05:52:13 +0200

    add back title to search view

--- lib/features/search/presentation/views/widgets/search_app_bar.dart
+++ lib/features/search/presentation/views/widgets/search_app_bar.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 class SearchAppBar extends StatelessWidget {
@@ -6,13 +7,24 @@ class SearchAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(left: 8, bottom: 8.0),
-      child: IconButton(
-        onPressed: () => Navigator.pop(context),
-        icon: const Icon(
-          Icons.arrow_back_sharp,
-          size: 25,
-        ),
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: Row(
+        children: [
+          IconButton(
+            padding: EdgeInsets.zero,
+            onPressed: () => Navigator.pop(context),
+            icon: const Icon(
+              Icons.arrow_back_sharp,
+              size: 25,
+            ),
+          ),
+          Text(
+            'Search',
+            style: Styles.testStyle20.copyWith(
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ],
       ),
     );
   }
