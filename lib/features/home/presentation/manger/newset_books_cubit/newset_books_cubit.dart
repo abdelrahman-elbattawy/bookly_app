1b4c3c41f0dcba20f153ae201fc516dd8da71ad0
Abdelrhman Elbattawy <abdo.elbattawy1st@gmail.com>
2024-02-29 04:31:39 +0200

    create newset books cubit

---
+++ lib/features/home/presentation/manger/newset_books_cubit/newset_books_cubit.dart
@@ -0,0 +1,23 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
+import 'package:equatable/equatable.dart';
+
+part 'newset_books_state.dart';
+
+class NewsetBooksCubit extends Cubit<NewsetBooksState> {
+  NewsetBooksCubit(this._homeRepo) : super(NewsetBooksInitial());
+
+  final HomeRepo _homeRepo;
+
+  Future<void> fetchNewsetBooks() async {
+    emit(NewsetBooksLoading());
+
+    var results = await _homeRepo.fetchNewestBooks();
+
+    results.fold(
+      (failure) => emit(NewsetBooksFailure(errMessage: failure.errMessage)),
+      (books) => emit(NewsetBooksSuccess(books: books)),
+    );
+  }
+}
