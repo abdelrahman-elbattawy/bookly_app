a6122c975196daf20b1077f56e4ca9621c2a99a2
Abdelrhman Elbattawy <abdo.elbattawy1st@gmail.com>
2024-02-29 03:01:32 +0200

    create api service

---
+++ lib/core/utils/api_service.dart
@@ -0,0 +1,14 @@
+import 'package:dio/dio.dart';
+
+class ApiService {
+  final _baseUrl = 'https://www.googleapis.com/books/v1/v';
+  final Dio dio;
+
+  ApiService({required this.dio});
+
+  Future<Map<String, dynamic>> get({required String endPoint}) async {
+    var respone = await dio.get('$_baseUrl$endPoint');
+
+    return respone.data;
+  }
+}
